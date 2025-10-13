a84f926e24fffba0f6e86b1b831b5c3be41e6476
felix.laarmann <felix.laarmann@tu-dortmund.de>
2025-10-13 16:32:51 +0200

    implement torch_algebra for utime

--- examples/auto_ml/utime.py
+++ examples/auto_ml/utime.py
@@ -1,11 +1,75 @@
+import numpy as np
 import torch
-from torch import nn
+import torch.nn as nn
+import torch.nn.functional as F
+
+from abc import ABC, abstractmethod
 
 from collections.abc import Container
 
 from cl3s import DSL, Constructor, Literal, Type, Var, SearchSpaceSynthesizer
 
 from typing import Any
+
+
+class ChannelWiseNormalization(nn.Module):
+    def __init__(self, num_channels, eps=1e-5):
+        super(ChannelWiseNormalization, self).__init__()
+        self.eps = eps
+        self.gamma = nn.Parameter(torch.ones(num_channels))
+        self.beta = nn.Parameter(torch.zeros(num_channels))
+
+    def forward(self, x):
+        # x has shape (batch_size, num_channels, time_steps)
+        mean = x.mean(dim=2, keepdim=True)
+        var = x.var(dim=2, keepdim=True, unbiased=False)
+        x_normalized = (x - mean) / torch.sqrt(var + self.eps)
+        x_scaled = self.gamma.view(1, -1, 1) * x_normalized + self.beta.view(1, -1, 1)
+        return x_scaled
+
+
+class Conv1dLayerNorm(nn.Module):
+    def __init__(self, num_channels):
+        super(Conv1dLayerNorm, self).__init__()
+        self.layer_norm = nn.LayerNorm(num_channels)
+
+    def forward(self, x):
+        # Permute to [batch_size, length, channels] for LayerNorm
+        x = x.permute(0, 2, 1)
+        x = self.layer_norm(x)
+        # Permute back to [batch_size, channels, length]
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class DepthwiseSeparableConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True):
+        super().__init__()
+
+        # Depthwise convolution: one filter per input channel
+        self.depthwise = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=in_channels,
+            bias=bias
+        )
+
+        # Pointwise (1x1) convolution: mixes channels
+        self.pointwise = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            bias=bias
+        )
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
 
 class UtimeRepository:
     def __init__(self, dimension_choices, normalization_eps_choices,
@@ -513,6 +577,85 @@ class UtimeRepository:
                                   & Constructor("dropout_p", Var("d"))
                                   & Constructor("normalization", Var("n"))
                                   & Constructor("normalization_epsilon", Var("e")))),
+        }
+
+    @staticmethod
+    def _conv_block(activation, dropout, c1, c2, norm, x):
+        x = c1(x)
+        x = norm(x)
+        x = activation(x)
+        x = c2(x)
+        x = norm(x)
+        x = activation(x)
+        x = dropout(x)
+        return x
+
+    @staticmethod
+    def _encoder(cb, mp, x):
+        y = cb(x)
+        x = mp(y)
+        if x.shape[-1] < 1:
+            raise ValueError("Encoder output is empty after pooling. Reduce the pooling size or number of layers.")
+        return x, y
+
+    @staticmethod
+    def _decoder(cb, up, x, skip):
+        x = up(x)
+        output_size = skip.size(2)
+
+        if x.size(2) != output_size:
+            diff = output_size - x.size(2)
+            x = F.pad(x, (0, diff))  # Apply zero padding to the end of the dimension
+
+        x = torch.cat([x, skip], dim=1)
+        x = cb(x)
+        return x
+
+    @staticmethod
+    def _umodel_length(enc, dec, cb, x, return_intermediate=False):
+        x, y = enc(x)
+        z = cb(x)
+        x = dec(z, y)
+        if return_intermediate:
+            return x, z.flatten(start_dim=1)
+        else:
+            return x
+
+    @staticmethod
+    def _umodel_cons_length(enc, dec, u_model, x, return_intermediate=False):
+        x, y = enc(x)
+        if return_intermediate:
+            z, u_intermediate = u_model(x, return_intermediate=return_intermediate)
+            x = dec(z, y)
+            return x, u_intermediate
+        else:
+            z = u_model(x, return_intermediate=return_intermediate)
+            x = dec(z, y)
+            return x
+
+
+    def torch_algebra(self):
+        return {
+            "ReLu": nn.ReLU(),
+            "ELU": nn.ELU(),
+            "Tanh": nn.Tanh(),
+            "BatchNorm1d": (lambda n: nn.BatchNorm1d(n)),
+            "ChannelWiseNorm": (lambda n, e: ChannelWiseNormalization(n, e)),
+            "Conv1dLayerNorm": (lambda n: Conv1dLayerNorm(n)),
+            "Dropout1d": (lambda d: nn.Dropout1d(p=d)),
+            "Maxpool1d": (lambda n: nn.MaxPool1d(n)),
+            "Upsample1d": (lambda n: nn.Upsample(scale_factor=n, mode="nearest")),
+            "Conv1d": (lambda i, o, k: nn.Conv1d(i, o, kernel_size=k, padding=k // 2)),
+            "DepthwiseSeparableConv1d": (lambda i, o, k: DepthwiseSeparableConv1d(i, o, kernel_size=k, padding=k // 2)),
+            "ConvBlock": (lambda i, o, k, d, af, c, e, activation, dropout, c1, c2, norm, x:
+                          self._conv_block(activation, dropout, c1, c2, norm, x)),
+            "Encoder": (lambda i, o, k, d, af, c, e, n, m, mp, cb, x: self._encoder(cb, mp, x)),
+            "Decoder": (lambda i, o, k, d, af, c, e, n, m, mp, cb, x, y: self._decoder(cb, mp, x, y)),
+            "Linear": (lambda i, o: nn.Linear(i, o)),
+            "UModel_length": (lambda i, out_enc, in_dec, k1, k2, d, af, c, e, n, m, enc, dec, cb, x:
+                              self._umodel_length(enc, dec, cb, x)),
+            "UModel_Cons_length": (lambda in_u, in_enc, in_dec, bd, k, bk, d, af, c, e, n, m, l, l_u, enc, dec, u_model, x:
+                                   self._umodel_cons_length(enc, dec, u_model, x)),
         }
 
 if __name__ == "__main__":
